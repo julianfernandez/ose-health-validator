3e0a1c4dba5d023a3bee0f25ab914e84f9140db4
xis15930 <julian.fernandez@servexternos.isban.es>
2016-06-10 14:50:07 +0200

    fix: running docker image into OSE

--- go/controller.go
+++ go/controller.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
-	"net/http"
-	"crypto/tls"
 	"io/ioutil"
-	"os"
 	"log"
+	"net/http"
+	"os"
 	"strconv"
 )
 
@@ -32,20 +32,21 @@ func Health(w http.ResponseWriter, r *http.Request) {
 func Validator(w http.ResponseWriter, r *http.Request) {
 
 	var token, apiServer string
+	var serviceArray []ServiceObject
+	isOSE := false
 	apiServerHost := os.Getenv("KUBERNETES_SERVICE_HOST")
 	apiServerPort := os.Getenv("KUBERNETES_SERVICE_PORT")
 	projectName := os.Getenv("PROJECT_NAME")
 
 	if apiServerHost == "" {
 		apiServer = "https://api.boae.paas.gsnetcloud.corp:8443"
-	}else {
+	} else {
 		apiServer = "https://" + apiServerHost + ":" + apiServerPort
 	}
-	
+
 	if projectName == "" {
 		projectName = "devstack-dev"
 	}
-	
 
 	// read the service account secret token file at once
 	tokenBytes, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/token")
@@ -54,6 +55,7 @@ func Validator(w http.ResponseWriter, r *http.Request) {
 		token = "uXQAIbJhQESrSwRzajEbUAorau6rPyyM7GC5o86Y7NE"
 	} else {
 		token = string(tokenBytes[:])
+		isOSE = true
 	}
 
 	transport := &http.Transport{
@@ -63,65 +65,23 @@ func Validator(w http.ResponseWriter, r *http.Request) {
 	cli := &http.Client{
 		Transport: transport,
 	}
-
-	// Set up the HTTP request to get Services
-	urlGetServices := apiServer + "/api/v1/namespaces/" + projectName + "/services"
-	req, err := http.NewRequest("GET", urlGetServices, nil)
-	req.Header.Add("Authorization", "Bearer "+token)
-
-	if err != nil {
-		panic(err)
-	}
-	
-	resp, err := cli.Do(req)
-	
-	if err != nil {
-		log.Println("Url Get Services=" + urlGetServices)
-		log.Fatal("Error getting Services")
-	}
-	
-	defer resp.Body.Close()
-
-	services, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	serviceArray := make([]ServiceObject, 0)
-	servicesCustom := map[string]interface{}{}
-	json.Unmarshal(services, &servicesCustom)
-
-	if servicesCustom != nil && len(servicesCustom)> 0 {
-		items := servicesCustom["items"].([]interface{})
-
-		for _, item := range items {
-			itemObject := item.(map[string]interface{})
-			metadataMap := itemObject["metadata"].(map[string]interface{})
-			specMap := itemObject["spec"].(map[string]interface{})
-			portMap := specMap["ports"].([]interface{})
-			portObject := portMap[0].(map[string]interface{})
-			urlService := "http://" + specMap["clusterIP"].(string) + ":" + strconv.FormatFloat(portObject["port"].(float64), 'f', 0, 64) + "/health"
-			//serviceArray = append(serviceArray, ServiceObject{metadataMap["name"].(string), urlService, ValidateService(cli, urlService, token)})
-			serviceArray = append(serviceArray, ServiceObject{metadataMap["name"].(string), urlService, "ok"})
-		}
-	}
 	
 	// Set up the HTTP request to get Routes
 	urlGetRoutes := apiServer + "/oapi/v1/namespaces/" + projectName + "/routes"
-	req, err = http.NewRequest("GET", urlGetRoutes, nil)
+	req, err := http.NewRequest("GET", urlGetRoutes, nil)
 	req.Header.Add("Authorization", "Bearer "+token)
 
 	if err != nil {
 		panic(err)
 	}
-	
-	resp, err = cli.Do(req)
-	
+
+	resp, err := cli.Do(req)
+
 	if err != nil {
 		log.Println("Url Get Routes=" + urlGetRoutes)
 		log.Fatal("Error getting Routes")
 	}
-	
+
 	defer resp.Body.Close()
 
 	routes, err := ioutil.ReadAll(resp.Body)
@@ -133,7 +93,7 @@ func Validator(w http.ResponseWriter, r *http.Request) {
 	routesCustom := map[string]interface{}{}
 	json.Unmarshal(routes, &routesCustom)
 
-	if routesCustom != nil && len(routesCustom)> 0 {
+	if routesCustom != nil && len(routesCustom) > 0 {
 		items := routesCustom["items"].([]interface{})
 
 		for _, item := range items {
@@ -150,12 +110,58 @@ func Validator(w http.ResponseWriter, r *http.Request) {
 			urlRoute := protocol + "://" + specMap["host"].(string) + "/health"
 			routeArray = append(routeArray, ServiceObject{metadataMap["name"].(string), urlRoute, ValidateService(cli, urlRoute, token)})
 		}
+	}	
+
+	//If Docker image in running into Openshift
+	if isOSE {
+		// Set up the HTTP request to get Services
+		urlGetServices := apiServer + "/api/v1/namespaces/" + projectName + "/services"
+		req, err = http.NewRequest("GET", urlGetServices, nil)
+		req.Header.Add("Authorization", "Bearer "+token)
+
+		if err != nil {
+			panic(err)
+		}
+
+		resp, err = cli.Do(req)
+
+		if err != nil {
+			log.Println("Url Get Services=" + urlGetServices)
+			log.Fatal("Error getting Services")
+		}
+
+		defer resp.Body.Close()
+
+		services, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		serviceArray = make([]ServiceObject, 0)
+		servicesCustom := map[string]interface{}{}
+		json.Unmarshal(services, &servicesCustom)
+
+		if servicesCustom != nil && len(servicesCustom) > 0 {
+			items := servicesCustom["items"].([]interface{})
+
+			for _, item := range items {
+				itemObject := item.(map[string]interface{})
+				metadataMap := itemObject["metadata"].(map[string]interface{})
+				specMap := itemObject["spec"].(map[string]interface{})
+				portMap := specMap["ports"].([]interface{})
+				portObject := portMap[0].(map[string]interface{})
+				urlService := "http://" + specMap["clusterIP"].(string) + ":" + strconv.FormatFloat(portObject["port"].(float64), 'f', 0, 64) + "/health"
+				serviceArray = append(serviceArray, ServiceObject{metadataMap["name"].(string), urlService, ValidateService(cli, urlService, token)})
+			}
+		}
 	}
-	
+
 	var validatorOutput ValidatorOutput
-	validatorOutput.Services = serviceArray
+	if isOSE{
+		validatorOutput.Services = serviceArray
+	}
 	validatorOutput.Routes = routeArray
-	
+
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(validatorOutput); err != nil {
