11baefe0800868cbe93c310a647c3d96015fb958
xis15930 <julian.fernandez@servexternos.isban.es>
2016-06-10 12:23:14 +0200

    Added build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+echo "Building health-validator docker image"
+
+TAG=$1
+PROXY=""
+
+if [ "$#" -gt 1 ]; then
+    PROXY="--build-arg https_proxy=$2"
+fi
+
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./image/health-validator ./go/*.go 
+
+if [ $? = 0 ]
+then
+	docker build ${PROXY} -t juliusmore/ose-health-validator:${TAG} -f ./image/Dockerfile ./image
+fi
