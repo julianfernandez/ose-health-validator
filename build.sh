b1fbaedf0c411faebc0aaecb8f25f42e7a409d1e
xis15930 <julian.fernandez@servexternos.isban.es>
2016-10-18 12:02:29 +0200

    compiling static files

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@ PROXY=""
 if [ "$#" -gt 1 ]; then
     PROXY="--build-arg https_proxy=$2"
 fi
-go generate esc -o ./go/static.go static
-go generate esc -o ./go/views.go views
+
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./image/health-validator ./go/*.go 
 
 if [ $? = 0 ]
