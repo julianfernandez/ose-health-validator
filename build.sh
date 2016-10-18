08b374489a23106f4a8dcc6e119a1416cdb55615
xis15930 <julian.fernandez@servexternos.isban.es>
2016-10-18 12:01:48 +0200

    compiling statuc files

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ PROXY=""
 if [ "$#" -gt 1 ]; then
     PROXY="--build-arg https_proxy=$2"
 fi
-
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./image/health-validator .
+go generate esc -o ./go/static.go static
+go generate esc -o ./go/views.go views
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./image/health-validator ./go/*.go 
 
 if [ $? = 0 ]
 then
